08b1276b2ee0b3224eacacf41c9a75f8d4f136a0
darkk <darkklaine@gmail.com>
2023-11-19 18:16:51 +0300

    бд

--- public/index.php
+++ public/index.php
@@ -1,18 +1,55 @@
 <?php
-//Задача:
-//На вход подается массив строк ($strings) и буква ($l).
-//На выход нужно передать массив, состоящий из всех строк, которые начинаются на переданную на вход букву ($l).
-//Регистр не должен учитываться.
-//Функция должна работать как с латиницей, так и с кирилицей.
 
-//lcfirst — Преобразует первый символ строки в нижний регистр
-//str_starts_with — Проверяет, начинается ли строка с заданной подстроки
-//array_filter — Фильтрует элементы массива с помощью callback-функции
+class DBConnection
+{
+    private string $servername = "localhost";
+    private string $username = "root";
+    private string $password = "pass";
+    private string $dbname = "history";
+    private mysqli $connection;
+
+    public function __construct()
+    {
+        $this->connection = new mysqli(
+            $this->servername,
+            $this->username,
+            $this->password,
+            $this->dbname,
+        );
+
+        if ($this->connection->connect_error) {
+            die("Ошибка подключения: " . $this->connection->connect_error);
+        }
+    }
+
+    public function getConnection(): mysqli
+    {
+        return $this->connection;
+    }
+
+    public function closeConnection(): void
+    {
+        $this->connection->close();
+        echo "Соединение закрыто." . PHP_EOL;
+    }
+}
+
+$databaseConnection = new DBConnection();
+$connection = $databaseConnection->getConnection();
+
+//$sqlInsert = "INSERT INTO `test` (`expression`, `date`) VALUES ('11+1=12', CURRENT_TIMESTAMP)";
 
 
-$input = ["Composer", "lpackages", "were", "лhanged.", "Лas", "Lupdated", "according", "to", "composer.json"];
-$l = "л";
+$sqlSelect = "SELECT expression, date FROM test ORDER BY date DESC LIMIT 5";
+$result = $connection->query($sqlSelect);
 
-$output = array_filter($input, fn($v) => str_starts_with(mb_strtolower($v), "$l"));
 
-var_dump($output);
+while ($row = $result->fetch_assoc()) {
+    $expression = $row["expression"];
+    $date = $row["date"];
+
+    echo $date . ' | ' . $expression . PHP_EOL;
+}
+
+
+$databaseConnection->closeConnection();
